46d2e33b0197f2dddf3b38fcc39c108c3140b2a7
Jake Leyhr <154226340+jakeleyhr@users.noreply.github.com>
2026-05-10 13:39:19 -0400

    Define Publication interface in publication.ts
    Added Publication interface with optional fields.

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -1,3 +1,16 @@
+export interface Publication {
+  year: string;
+  conference: string;
+  title: string;
+  authors: string;
+  paperUrl?: string;
+  codeUrl?: string;
+  bibtex?: string;
+  tldr?: string;
+  imageUrl?: string;
+  award?: string;
+}
+
 export const publicationData: Publication[] = [
   {
     year: "2026",
